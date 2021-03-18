e36e110f8f0930b20bc955515de36b8076b40416
Tobias Nienhaus <tobiasnienhaus69@gmail.com>
2021-03-18 12:17:32 +0100

    Try out gluon
    Somehow, I always get a stackoverflow with it

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,50 @@ fn main() {
 }
 
 fn gluon_test() {
+    use gluon::*;
+    use gluon::vm::api::IO;
+    use gluon::import::*;
+    // let mut vm = new_vm();
+    // vm.run_io(true);
+    // let script = r#"
+    // let io = import! std.io
+    // io.print "123"
+    // "#;
+    // // vm.run_expr::<IO<()>>(&vm, "test", script).unwrap();
+    // // let (result, _): (api::IO<()>, _) = vm.run_expr("test_script", script).unwrap();
+    // // let _: (api::IO<()>, _) = vm.run_expr("test_script", script).unwrap();
+    // // vm.run_expr::<IO<()>>("example", script)
+    // // .unwrap();
+    // // Prints `123` to stdout
+    // vm.run_io(true);
+    // vm.run_expr::<IO<()>>("example", script)
+    //     .unwrap();
+    fn factorial(x: i32) -> i32 {
+        if x <= 1 {
+            1
+        } else {
+            x * factorial(x - 1)
+        }
+    }
+
+    fn load_factorial(vm: &Thread) -> vm::Result<vm::ExternModule> {
+        vm::ExternModule::new(vm, primitive!(1, factorial))
+    }
+
+    let vm = new_vm();
+
+    // Introduce a module that can be loaded with `import! factorial`
+    add_extern_module(&vm, "factorial", load_factorial);
+
+    let expr = r#"
+        let factorial = import! factorial
+        factorial 5
+    "#;
+    println!("Before execution");
+    let (result, _) = vm.run_expr::<i32>("factorial", expr)
+        .unwrap();
+
+    assert_eq!(result, 120);
 }
 
 fn rhai_test() {
